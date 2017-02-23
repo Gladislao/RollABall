13f3642af6ad25a8664c74ff1fac9b32e01432de
Gladislao <gcuzcanor@gmail.com>
2017-02-23 11:52:52 -0500

    Add game

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = -Input.acceleration.z;
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+		rb.AddForce (movement);
+
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text="Count: " +count.ToString();
+		if (count >= 12) {
+			winText.text = "You Win!";
+		}
+	}
+}
